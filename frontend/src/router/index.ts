e10e0eee5505c6ed9e3a599dd8ae3589428ef178
daniuda <fddimpex@yahoo.com>
2026-05-06 08:33:21 +0300

    Deploy pe /dex/: base URL, router history, workflow pe master, nginx config

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import SwapView from '../views/SwapView.vue'
 
 export const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/dex/'),
   routes: [
     { path: '/', component: SwapView },
     { path: '/:pathMatch(.*)*', redirect: '/' },
